a30c4a585175f417d2501177d79b9e4e85a9c008
RobertGMcLendon <robertgmclendon@gmail.com>
2025-07-29 18:01:40 +0000

    Fix Vite entry config for index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Tell Vite to treat root folder as base
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'), // Explicit entry file
+    },
+  },
 });
