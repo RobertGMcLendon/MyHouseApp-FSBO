e616ec138028235aa5f926cf16ef5c67bfdb2e17
RobertGMcLendon <robertgmclendon@gmail.com>
2025-07-31 03:19:44 +0000

    Added Google verification file

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Tell Vite to treat root folder as base
+  root: '.',
   build: {
     rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'), // Explicit entry file
+      input: 'index.html', // Use relative path directly
     },
   },
 });
